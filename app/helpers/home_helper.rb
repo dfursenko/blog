634b24ee5cdf670844e47b0315e2433c8320f889
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-24 00:52:04 +0700

    Новый дизайн

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,7 @@
 module HomeHelper
+  # Returns the Gravatar for the given user.
+  def gravatar_author
+    gravatar_id = Digest::MD5::hexdigest('dfursenko@gmail.com')
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=70"
+  end
 end
