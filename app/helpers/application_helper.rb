3696e9a65207d4ba1ad52ced1cee8243dea2358e
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-25 00:03:11 +0700

    pluralize comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def comments_pluralize(count)
+    def my_plural(count, one, two, three)
+      i = count.to_s.split('').last.to_i
+      if i == 1
+        one
+      elsif [2, 3, 4].include? i
+        two
+      else
+        three
+      end
+    end
+
+    my_plural(count, 'комментарий', 'комментария', 'комментариев')
+  end
 end
