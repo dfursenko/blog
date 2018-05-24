44e3f0566ff6125f175ff8dc229280cb4ea46750
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-24 18:15:10 +0700

    Статическая страница About

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+    # @categories = Category.all
+    # @tags = Tag.all
+    @articles = Article.all.order('created_at DESC')
+  end
+
+  def about
 
   end
 end
