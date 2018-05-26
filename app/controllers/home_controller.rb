84afccb12f790c20489a1938de87fa062ad66dda
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-26 23:34:41 +0700

    HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,17 @@
 class HomeController < ApplicationController
   def index
-    # @categories = Category.all
-    # @tags = Tag.all
-    @articles = Article.all.order('created_at DESC')
+    @articles =
+      if params[:category]
+        category_id = Category.find_by(name: params[:category]).id
+        Article.where(category_id: category_id).order('created_at DESC')
+      else
+        Article.all.order('created_at DESC')
+      end
+    @articles_last = Article.all.limit(4).order('created_at DESC')
+    @categories = Category.all
+    @tags = Tag.all
   end
 
   def about
-
   end
 end
