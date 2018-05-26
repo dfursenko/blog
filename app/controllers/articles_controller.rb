261f21784ffbdd66000749035db059c4c12cf798
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-27 01:58:28 +0700

    Сохранение тегов в базу

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -70,13 +70,13 @@ class ArticlesController < ApplicationController
   # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def article_params
-      params.require(:article).permit(:title, :text, :image, :category_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def article_params
+    params.require(:article).permit(:title, :text, :image, :category_id, tag_ids: [])
+  end
 end
