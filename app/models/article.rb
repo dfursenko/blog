14518ea5d0174e93a6fa2c167036f19c861a9bf2
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-28 16:19:16 +0700

    Валидация

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
   has_and_belongs_to_many :tags
   has_many :comments
   validates :title, presence: true, length: { minimum: 5 }
+  validates :text, presence: true, length: { minimum: 50 }
 end
