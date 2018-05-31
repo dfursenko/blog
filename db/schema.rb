17b8e495fac20c6c139e035c475fb9c1237cf403
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-31 23:18:47 +0700

    add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524091724) do
+ActiveRecord::Schema.define(version: 20180531133456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 20180524091724) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_articles_on_category_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "articles_tags", id: false, force: :cascade do |t|
@@ -47,6 +49,16 @@ ActiveRecord::Schema.define(version: 20180524091724) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "seos", force: :cascade do |t|
+    t.string "description"
+    t.string "keywords"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "seoble_type"
+    t.bigint "seoble_id"
+    t.index ["seoble_type", "seoble_id"], name: "index_seos_on_seoble_type_and_seoble_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -60,6 +72,19 @@ ActiveRecord::Schema.define(version: 20180524091724) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.boolean "moderator"
+    t.boolean "creator"
+    t.boolean "banned"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+  end
+
   add_foreign_key "articles", "categories"
+  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end
