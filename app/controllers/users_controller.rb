b94cc5dfc71d8522d550d099ad23ababa0432e6e
Dmitry Fursenko <dfursenko@gmail.com>
2018-06-07 19:42:20 +0700

    Профиль пользователя

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :profile, :profile_edit, :profile_update]
 
   # GET /users
   # GET /users.json
@@ -10,6 +10,19 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+  end
+
+  def profile
+    # @user = User.find_by(name: params[:name])
+  end
+
+  def profile_edit
+    # @user = User.find_by(name: params[:name])
+    # redirect_to user_profile_path(@user), notice: 'Профиль успешно изменен'
+  end
+
+  def profile_update
+    redirect_to user_profile_path(@user), notice: 'Данные успешно изменены.' if @user.update(user_params)
   end
 
   # GET /users/new
@@ -69,6 +82,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :moderator, :creator, :banned)
+      params.require(:user).permit(:name, :password, :email, :moderator, :creator, :banned)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users
   resources :categories
   resources :articles do
@@ -8,4 +9,8 @@ Rails.application.routes.draw do
   # get  'home/index', to: '/'
   get  'home/about'
   root 'home#index'
+
+  get "/profile/:id(.:format)", to: "users#profile", as: 'user_profile'
+  get "/profile/edit/:id(.:format)", to: "users#profile_edit", as: 'user_profile_edit'
+  patch "/profile/update/:id(.:format)", to: "users#profile_update", as: 'user_profile_update'
 end
